bc27e546881adb775901ef86762fa54877576163
velasvalen17 <velasvalen17@gmail.com>
2020-04-24 18:08:43 +0200

    Updates

---
+++ precache-manifest.7c7424e4be93e24f467764f7fde8ddd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2a9e775c5269fd499c99ea3cdd3fd2",
+    "url": "/covid19-dashboard/index.html"
+  },
+  {
+    "revision": "4e81d6bb263939a400a9",
+    "url": "/covid19-dashboard/static/css/main.a7083161.chunk.css"
+  },
+  {
+    "revision": "ddcd786e6e270aca524d",
+    "url": "/covid19-dashboard/static/js/2.84323c0a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covid19-dashboard/static/js/2.84323c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e81d6bb263939a400a9",
+    "url": "/covid19-dashboard/static/js/main.db4714bc.chunk.js"
+  },
+  {
+    "revision": "50048fa19b0b7d50b055",
+    "url": "/covid19-dashboard/static/js/runtime-main.a1905b9d.js"
+  }
+]);
