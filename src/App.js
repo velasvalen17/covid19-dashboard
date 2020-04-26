4fd4fe14be76d8235b6ebb83eb58871c8b0fd6e0
velasvalen17 <velasvalen17@gmail.com>
2020-04-26 13:33:35 +0200

    style and responsive changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
 
+import banner from "./images/banner.png";
+
 class App extends React.Component {
   state = {
     data: {},
@@ -27,6 +29,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={banner} alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
